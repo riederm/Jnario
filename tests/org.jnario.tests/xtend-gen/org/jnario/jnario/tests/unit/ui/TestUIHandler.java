cd156e4e02ff2c35fe13883d3bbc9e1a3bcc5b95
Boris Brodski <brodsky_boris@yahoo.com>
2016-05-12 19:53:08 +0200

    Tests: Remove version constrains for Jnario, regenerate Xtend.

--- tests/org.jnario.tests/xtend-gen/org/jnario/jnario/tests/unit/ui/TestUIHandler.java
+++ tests/org.jnario.tests/xtend-gen/org/jnario/jnario/tests/unit/ui/TestUIHandler.java
@@ -1,12 +1,14 @@
 package org.jnario.jnario.tests.unit.ui;
 
 import org.eclipse.jdt.junit.model.ITestElement;
+import org.eclipse.jface.action.Action;
 import org.eclipse.ui.part.ViewPart;
+import org.jnario.ui.handler.AbstractJnarioRunnerUIHandler;
 
 @SuppressWarnings("all")
-public class TestUIHandler /* implements AbstractJnarioRunnerUIHandler  */{
+public class TestUIHandler extends AbstractJnarioRunnerUIHandler {
   @Override
-  public Object getAction(final ViewPart part, final ITestElement element) {
+  public Action getAction(final ViewPart part, final ITestElement element) {
     return null;
   }
 }
