74b5969a1fc61d9fdff1190f41125df888980c62
Boris Brodski <brodsky_boris@yahoo.com>
2016-05-12 15:29:03 +0200

    Minor changes

--- tests/org.jnario.tests/xtend-gen/org/jnario/jnario/tests/unit/ui/TestUIHandler.java
+++ tests/org.jnario.tests/xtend-gen/org/jnario/jnario/tests/unit/ui/TestUIHandler.java
@@ -1,14 +1,12 @@
 package org.jnario.jnario.tests.unit.ui;
 
 import org.eclipse.jdt.junit.model.ITestElement;
-import org.eclipse.jface.action.Action;
 import org.eclipse.ui.part.ViewPart;
-import org.jnario.ui.handler.AbstractJnarioRunnerUIHandler;
 
 @SuppressWarnings("all")
-public class TestUIHandler extends AbstractJnarioRunnerUIHandler {
+public class TestUIHandler /* implements AbstractJnarioRunnerUIHandler  */{
   @Override
-  public Action getAction(final ViewPart part, final ITestElement element) {
+  public Object getAction(final ViewPart part, final ITestElement element) {
     return null;
   }
 }
