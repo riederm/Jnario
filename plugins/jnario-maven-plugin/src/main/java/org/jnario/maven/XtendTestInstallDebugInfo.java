9862834969b6af7a68e989fb3d4b1719e993db8e
Ghaith  Hachem <ghaith.hachem@gmail.com>
2017-05-29 09:08:42 +0200

    Added XtendTestInstallDebugInfo for compatibiltiy with old versions

---
+++ plugins/jnario-maven-plugin/src/main/java/org/jnario/maven/XtendTestInstallDebugInfo.java
@@ -0,0 +1,21 @@
+package org.jnario.maven;
+
+import static org.apache.maven.plugins.annotations.LifecyclePhase.PROCESS_TEST_CLASSES;
+import static org.apache.maven.plugins.annotations.ResolutionScope.COMPILE;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.maven.plugins.annotations.Mojo;
+
+import com.google.common.collect.Multimap;
+
+/**
+ * Goal which installs debug information into Java class files that have been generated from Jnario.
+ * @deprecated use org.jnario.maven.JnarioTestInstallDebugInfo 
+ * @author Moritz Eysholdt - Initial contribution and API
+ */
+@Mojo(name = "xtend-test-install-debug-info", defaultPhase = PROCESS_TEST_CLASSES, requiresDependencyResolution = COMPILE)
+@Deprecated("use org.jnario.maven.JnarioTestInstallDebugInfo")
+public class XtendTestInstallDebugInfo extends JnarioTestInstallDebugInfo {
+}
